7febeebef282e1b90835b8b3f6432c0b75439056
Pablo Gutiérrez Félix <pablogf@uma.es>
2025-04-10 17:27:44 +0200

    Changed JSON format and added gh-pages
    Signed-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>

--- scripts/parse_liboqs_speed.py
+++ scripts/parse_liboqs_speed.py
@@ -9,15 +9,15 @@ class State(Enum):
    config=1
    parsing=2
 
-data={}
+data = {}
 
-if len(sys.argv)!=2:
+if len(sys.argv) != 2:
    print("Usage: %s <logfile to parse>" % (sys.argv[0]))
    exit(-1)
 
 fn = sys.argv[1]
 state = State.starting
-alg=""
+alg = ""
 
 with open(fn) as fp: 
    while True:
@@ -26,42 +26,55 @@ with open(fn) as fp:
             break 
       # Remove newlines
       line = line.rstrip()
-      if state==State.starting:
+      if state == State.starting:
            if line.startswith("Configuration info"):
-             state=State.config
-             data["config"]={}
+             state = State.config
+             data["config"] = {}
              fp.readline()
-      elif state==State.config:
-             if line=="\n": # Skip forward
+      elif state == State.config:
+             if line == "\n": # Skip forward
                fp.readline()
                fp.readline()
              if line.startswith("-------"):
-                state=State.parsing
+                state = State.parsing
              elif line.startswith("Started at"):
                 data["config"]["start"] = line[len("Started at "):]
                 fp.readline()
              elif ":" in line:
-                data["config"][line[:line.index(":")]]=line[line.index(":")+1:].lstrip() 
-      elif state==State.parsing:
+                data["config"][line[:line.index(":")]] = line[line.index(":")+1:].lstrip() 
+      elif state == State.parsing:
            if line.startswith("Ended"): # Finish
               break
            else:
                alg = line[:line.index(" ")]
-               data[alg]={}
+               data[alg] = {}
                p = re.compile('\S+\s*\|')
-               for i in 0,1,2:
-                 x=p.findall(fp.readline().rstrip())
+               for i in 0, 1, 2:
+                 x = p.findall(fp.readline().rstrip())
                  tag = x[0][:x[0].index(" ")]
-                 ctag = tag+"cycles"
+                 ctag = tag + "cycles"
                  iterations = float(x[1][:x[1].index(" ")])
                  t = float(x[2][:x[2].index(" ")])
                  cycles = float(x[5][:x[5].index(" ")])
-                 val = iterations/t
-                 data[alg][tag]=round(val,2)
-                 data[alg][ctag]=int(cycles)
+                 val = iterations / t
+                 data[alg][tag] = round(val, 2)
+                 data[alg][ctag] = int(cycles)
       else:
            print("Unknown state: %s" % (line))
 
-# Dump data
-with open(os.path.splitext(fn)[0]+".json", 'w') as outfile:
-    json.dump(data, outfile)
+# Transform data into the required format
+output = []
+for alg, metrics in data.items():
+    if alg == "config":
+        continue
+    for metric, value in metrics.items():
+        entry = {
+            "name": f"{alg} - {metric}",
+            "unit": "Microseconds",
+            "value": value
+        }
+        output.append(entry)
+
+# Dump transformed data
+with open(os.path.splitext(fn)[0] + "_formatted.json", 'w') as outfile:
+    json.dump(output, outfile, indent=4)
