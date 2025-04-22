b556ca0e25830ef9ae0112b2f1dece6ca34a73d8
github-action-benchmark <github@users.noreply.github.com>
2025-04-22 10:40:04 +0000

    add BIKE-L1 (customSmallerIsBetter) benchmark result for 789fe2a336972c9a9a625832a9f4f4f3b986c892

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745318402370,
+  "lastUpdate": 1745318404160,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "Classic-McEliece-8192128f": [
@@ -612,6 +612,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "FrodoKEM-1344-SHAKE decaps",
             "value": 10535216,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ],
+    "BIKE-L1": [
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "789fe2a336972c9a9a625832a9f4f4f3b986c892",
+          "message": "no artifacts added\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-22T12:36:38+02:00",
+          "tree_id": "44db001064c0afbe46350b539d0d3242b49d269e",
+          "url": "https://github.com/pablo-gf/liboqs/commit/789fe2a336972c9a9a625832a9f4f4f3b986c892"
+        },
+        "date": 1745318389397,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "BIKE-L1 keygen",
+            "value": 661648,
+            "unit": "cycles"
+          },
+          {
+            "name": "BIKE-L1 encaps",
+            "value": 107065,
+            "unit": "cycles"
+          },
+          {
+            "name": "BIKE-L1 decaps",
+            "value": 1902589,
             "unit": "cycles"
           }
         ]
