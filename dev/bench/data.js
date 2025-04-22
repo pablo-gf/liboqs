d67aeafa97d68783e2f08480d2d4f65dfe27f011
github-action-benchmark <github@users.noreply.github.com>
2025-04-22 10:39:57 +0000

    add FrodoKEM-976-SHAKE (customSmallerIsBetter) benchmark result for 789fe2a336972c9a9a625832a9f4f4f3b986c892

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745318396273,
+  "lastUpdate": 1745318397394,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "Classic-McEliece-8192128f": [
@@ -448,6 +448,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "Classic-McEliece-8192128 decaps",
             "value": 254659,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ],
+    "FrodoKEM-976-SHAKE": [
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "789fe2a336972c9a9a625832a9f4f4f3b986c892",
+          "message": "no artifacts added\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-22T12:36:38+02:00",
+          "tree_id": "44db001064c0afbe46350b539d0d3242b49d269e",
+          "url": "https://github.com/pablo-gf/liboqs/commit/789fe2a336972c9a9a625832a9f4f4f3b986c892"
+        },
+        "date": 1745318386204,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "FrodoKEM-976-SHAKE keygen",
+            "value": 5651554,
+            "unit": "cycles"
+          },
+          {
+            "name": "FrodoKEM-976-SHAKE encaps",
+            "value": 6025988,
+            "unit": "cycles"
+          },
+          {
+            "name": "FrodoKEM-976-SHAKE decaps",
+            "value": 5879680,
             "unit": "cycles"
           }
         ]
