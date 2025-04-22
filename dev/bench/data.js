cd6354a8b5ece26a569f695390728475214565a5
github-action-benchmark <github@users.noreply.github.com>
2025-04-22 10:39:53 +0000

    add Classic-McEliece-460896 (customSmallerIsBetter) benchmark result for 789fe2a336972c9a9a625832a9f4f4f3b986c892

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745318392389,
+  "lastUpdate": 1745318393456,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "Classic-McEliece-8192128f": [
@@ -325,6 +325,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "Classic-McEliece-348864f decaps",
             "value": 93707,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ],
+    "Classic-McEliece-460896": [
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "789fe2a336972c9a9a625832a9f4f4f3b986c892",
+          "message": "no artifacts added\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-22T12:36:38+02:00",
+          "tree_id": "44db001064c0afbe46350b539d0d3242b49d269e",
+          "url": "https://github.com/pablo-gf/liboqs/commit/789fe2a336972c9a9a625832a9f4f4f3b986c892"
+        },
+        "date": 1745318383039,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Classic-McEliece-460896 keygen",
+            "value": 308226826,
+            "unit": "cycles"
+          },
+          {
+            "name": "Classic-McEliece-460896 encaps",
+            "value": 79232,
+            "unit": "cycles"
+          },
+          {
+            "name": "Classic-McEliece-460896 decaps",
+            "value": 227110,
             "unit": "cycles"
           }
         ]
