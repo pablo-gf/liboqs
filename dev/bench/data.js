eb216974b6835170b4cb09735c180debbc5cb8f2
github-action-benchmark <github@users.noreply.github.com>
2025-04-25 09:44:43 +0000

    add Falcon-1024 (customSmallerIsBetter) benchmark result for 660f01d300dcf778a82d7787ee00fdfa8f78be1f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745574282155,
+  "lastUpdate": 1745574283421,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L5": [
@@ -1012,6 +1012,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "cross-rsdp-128-small verify",
             "value": 2767606,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ],
+    "Falcon-1024": [
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "660f01d300dcf778a82d7787ee00fdfa8f78be1f",
+          "message": "added speed_sig\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-25T11:38:08+02:00",
+          "tree_id": "bb29d3d75cdc5a640140a80df865c33ff900e286",
+          "url": "https://github.com/pablo-gf/liboqs/commit/660f01d300dcf778a82d7787ee00fdfa8f78be1f"
+        },
+        "date": 1745574277096,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Falcon-1024 keypair",
+            "value": 50199745,
+            "unit": "cycles"
+          },
+          {
+            "name": "Falcon-1024 sign",
+            "value": 1126815,
+            "unit": "cycles"
+          },
+          {
+            "name": "Falcon-1024 verify",
+            "value": 204141,
             "unit": "cycles"
           }
         ]
