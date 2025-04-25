6c21fd9a5c68bc769b15c0b92c8ecb12a65b0b13
github-action-benchmark <github@users.noreply.github.com>
2025-04-25 09:41:35 +0000

    add FrodoKEM-1344-AES (customSmallerIsBetter) benchmark result for 660f01d300dcf778a82d7787ee00fdfa8f78be1f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745574094163,
+  "lastUpdate": 1745574095071,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L5": [
@@ -79,6 +79,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "FrodoKEM-1344-AES decaps",
             "value": 3118600,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "660f01d300dcf778a82d7787ee00fdfa8f78be1f",
+          "message": "added speed_sig\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-25T11:38:08+02:00",
+          "tree_id": "bb29d3d75cdc5a640140a80df865c33ff900e286",
+          "url": "https://github.com/pablo-gf/liboqs/commit/660f01d300dcf778a82d7787ee00fdfa8f78be1f"
+        },
+        "date": 1745574086522,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "FrodoKEM-1344-AES keygen",
+            "value": 2409619,
+            "unit": "cycles"
+          },
+          {
+            "name": "FrodoKEM-1344-AES encaps",
+            "value": 3298366,
+            "unit": "cycles"
+          },
+          {
+            "name": "FrodoKEM-1344-AES decaps",
+            "value": 3120945,
             "unit": "cycles"
           }
         ]
