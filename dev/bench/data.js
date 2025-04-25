793a230fb53d7f46f8bb28c7637187d30522aded
github-action-benchmark <github@users.noreply.github.com>
2025-04-25 09:41:35 +0000

    add FrodoKEM-976-AES (customSmallerIsBetter) benchmark result for 660f01d300dcf778a82d7787ee00fdfa8f78be1f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745574095071,
+  "lastUpdate": 1745574095867,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L5": [
@@ -487,6 +487,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "Classic-McEliece-460896 decaps",
             "value": 226563,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ],
+    "FrodoKEM-976-AES": [
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "660f01d300dcf778a82d7787ee00fdfa8f78be1f",
+          "message": "added speed_sig\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-25T11:38:08+02:00",
+          "tree_id": "bb29d3d75cdc5a640140a80df865c33ff900e286",
+          "url": "https://github.com/pablo-gf/liboqs/commit/660f01d300dcf778a82d7787ee00fdfa8f78be1f"
+        },
+        "date": 1745574089374,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "FrodoKEM-976-AES keygen",
+            "value": 1473351,
+            "unit": "cycles"
+          },
+          {
+            "name": "FrodoKEM-976-AES encaps",
+            "value": 1977145,
+            "unit": "cycles"
+          },
+          {
+            "name": "FrodoKEM-976-AES decaps",
+            "value": 1835094,
             "unit": "cycles"
           }
         ]
