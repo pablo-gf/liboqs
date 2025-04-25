1ab8fcd3c1ae262e3c7318a3080c4734e0281668
github-action-benchmark <github@users.noreply.github.com>
2025-04-25 10:43:47 +0000

    add Classic-McEliece-348864 (customSmallerIsBetter) benchmark result for f0f8114c78832ad7be392b49ea59ce5d6aa5e412

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745577826501,
+  "lastUpdate": 1745577827345,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "Classic-McEliece-6688128f": [
@@ -444,6 +444,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Classic-McEliece-348864 decaps",
             "value": 94651,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "f0f8114c78832ad7be392b49ea59ce5d6aa5e412",
+          "message": "updated artifact handling version\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-25T12:40:36+02:00",
+          "tree_id": "e4bc585c4198457b041535ef3616c84a304df4ec",
+          "url": "https://github.com/pablo-gf/liboqs/commit/f0f8114c78832ad7be392b49ea59ce5d6aa5e412"
+        },
+        "date": 1745577818586,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Classic-McEliece-348864 keygen",
+            "value": 94213221,
+            "unit": "cycles"
+          },
+          {
+            "name": "Classic-McEliece-348864 encaps",
+            "value": 36390,
+            "unit": "cycles"
+          },
+          {
+            "name": "Classic-McEliece-348864 decaps",
+            "value": 94316,
             "unit": "cycles"
           }
         ]
