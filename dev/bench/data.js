7335b50fe955df9679edf4f88007fac618a9e637
github-action-benchmark <github@users.noreply.github.com>
2025-04-25 20:56:00 +0000

    add BIKE-L1 (customSmallerIsBetter) benchmark result for 56e5cf25057c8ea6f9afad441424b6ef343d9a71

---
+++ dev/bench/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1745614560628,
+  "repoUrl": "https://github.com/pablo-gf/liboqs",
+  "entries": {
+    "BIKE-L1": [
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "56e5cf25057c8ea6f9afad441424b6ef343d9a71",
+          "message": "add fetch-depth 0 (2)\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-25T22:51:50+02:00",
+          "tree_id": "02f0b09a85bf22b19d342659045dd06844791348",
+          "url": "https://github.com/pablo-gf/liboqs/commit/56e5cf25057c8ea6f9afad441424b6ef343d9a71"
+        },
+        "date": 1745614559659,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "BIKE-L1 keygen",
+            "value": 651050,
+            "unit": "cycles"
+          },
+          {
+            "name": "BIKE-L1 encaps",
+            "value": 105368,
+            "unit": "cycles"
+          },
+          {
+            "name": "BIKE-L1 decaps",
+            "value": 1854810,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ]
+  }
+}
