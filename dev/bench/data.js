58fd3a8117d97e187ca0359ba70325afa6f2fc8b
github-action-benchmark <github@users.noreply.github.com>
2025-04-25 11:31:24 +0000

    add Classic-McEliece-8192128f (customSmallerIsBetter) benchmark result for 2f4a6ecf541d675947a971e1a3d5f541e580aee4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745580682984,
+  "lastUpdate": 1745580684012,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "Classic-McEliece-348864f": [
@@ -366,6 +366,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "FrodoKEM-1344-AES decaps",
             "value": 3126244,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ],
+    "Classic-McEliece-8192128f": [
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "2f4a6ecf541d675947a971e1a3d5f541e580aee4",
+          "message": "auto-push set to false\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-25T13:28:03+02:00",
+          "tree_id": "f67c86f791ccc7ed22c7021feb72a53bcc8d6293",
+          "url": "https://github.com/pablo-gf/liboqs/commit/2f4a6ecf541d675947a971e1a3d5f541e580aee4"
+        },
+        "date": 1745580676156,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Classic-McEliece-8192128f keygen",
+            "value": 354578089,
+            "unit": "cycles"
+          },
+          {
+            "name": "Classic-McEliece-8192128f encaps",
+            "value": 150796,
+            "unit": "cycles"
+          },
+          {
+            "name": "Classic-McEliece-8192128f decaps",
+            "value": 254354,
             "unit": "cycles"
           }
         ]
