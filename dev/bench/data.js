a043ad0a292d6c318d3ac865c9ae7726fe2eee1d
github-action-benchmark <github@users.noreply.github.com>
2025-04-25 10:53:13 +0000

    add FrodoKEM-1344-AES (customSmallerIsBetter) benchmark result for a30a486321b148aed0935fad7f4cf54805351910

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745578392033,
+  "lastUpdate": 1745578393095,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "Classic-McEliece-460896f": [
@@ -530,6 +530,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "FrodoKEM-640-AES decaps",
             "value": 950781,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ],
+    "FrodoKEM-1344-AES": [
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "a30a486321b148aed0935fad7f4cf54805351910",
+          "message": "updated artifacts to v4\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-25T12:49:46+02:00",
+          "tree_id": "b235591e163a4ab2db7f4b6e45ca7caccd9aee64",
+          "url": "https://github.com/pablo-gf/liboqs/commit/a30a486321b148aed0935fad7f4cf54805351910"
+        },
+        "date": 1745578378126,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "FrodoKEM-1344-AES keygen",
+            "value": 2411793,
+            "unit": "cycles"
+          },
+          {
+            "name": "FrodoKEM-1344-AES encaps",
+            "value": 3280307,
+            "unit": "cycles"
+          },
+          {
+            "name": "FrodoKEM-1344-AES decaps",
+            "value": 3125398,
             "unit": "cycles"
           }
         ]
