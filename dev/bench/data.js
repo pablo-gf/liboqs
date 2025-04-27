92c9524d3ba504abb18481886cacf74130e7d322
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 17:34:43 +0000

    add OV-V-pkc-skc (customSmallerIsBetter) benchmark result for 02ec569c088c8f7a14e3602685faf72f73aa0d66

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745775256086,
+  "lastUpdate": 1745775283537,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L1": [
@@ -9716,6 +9716,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "OV-V-pkc-skc verify",
             "value": 1526929,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "02ec569c088c8f7a14e3602685faf72f73aa0d66",
+          "message": "cleaned kem-bench\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-27T19:05:19+02:00",
+          "tree_id": "ce6b6bfa03f4ff3a38999ba463955eb719111968",
+          "url": "https://github.com/pablo-gf/liboqs/commit/02ec569c088c8f7a14e3602685faf72f73aa0d66"
+        },
+        "date": 1745775282910,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "OV-V-pkc-skc keypair",
+            "value": 23893397,
+            "unit": "cycles"
+          },
+          {
+            "name": "OV-V-pkc-skc sign",
+            "value": 13298725,
+            "unit": "cycles"
+          },
+          {
+            "name": "OV-V-pkc-skc verify",
+            "value": 1559125,
             "unit": "cycles"
           }
         ]
