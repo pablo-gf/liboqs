261bef0f8997c2a00e362f089e317b7a3ef02086
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 16:27:40 +0000

    add OV-Ip (customSmallerIsBetter) benchmark result for 781af869157db7541100d008135143b25c3d69f7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745771230400,
+  "lastUpdate": 1745771260131,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L1": [
@@ -5718,6 +5718,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "OV-Ip verify",
             "value": 57819,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "781af869157db7541100d008135143b25c3d69f7",
+          "message": "cleaned sig-bench\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-27T18:02:40+02:00",
+          "tree_id": "2f686f0987c9203d98ad9a4baf678ebb30bb1525",
+          "url": "https://github.com/pablo-gf/liboqs/commit/781af869157db7541100d008135143b25c3d69f7"
+        },
+        "date": 1745771258904,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "OV-Ip keypair",
+            "value": 1577641,
+            "unit": "cycles"
+          },
+          {
+            "name": "OV-Ip sign",
+            "value": 59169,
+            "unit": "cycles"
+          },
+          {
+            "name": "OV-Ip verify",
+            "value": 73800,
             "unit": "cycles"
           }
         ]
