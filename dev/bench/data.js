4ccbcdb98e7ea7f995b9033489c9dee1ed9932c9
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 17:23:19 +0000

    add cross-rsdp-192-small (customSmallerIsBetter) benchmark result for 02ec569c088c8f7a14e3602685faf72f73aa0d66

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745774573652,
+  "lastUpdate": 1745774599410,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L1": [
@@ -6860,6 +6860,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "cross-rsdp-192-small verify",
             "value": 4361441,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "02ec569c088c8f7a14e3602685faf72f73aa0d66",
+          "message": "cleaned kem-bench\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-27T19:05:19+02:00",
+          "tree_id": "ce6b6bfa03f4ff3a38999ba463955eb719111968",
+          "url": "https://github.com/pablo-gf/liboqs/commit/02ec569c088c8f7a14e3602685faf72f73aa0d66"
+        },
+        "date": 1745774598566,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "cross-rsdp-192-small keypair",
+            "value": 103882,
+            "unit": "cycles"
+          },
+          {
+            "name": "cross-rsdp-192-small sign",
+            "value": 6205849,
+            "unit": "cycles"
+          },
+          {
+            "name": "cross-rsdp-192-small verify",
+            "value": 4375940,
             "unit": "cycles"
           }
         ]
