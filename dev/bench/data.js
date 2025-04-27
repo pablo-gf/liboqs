c28ec430abbf154e28838f3c31123070f04b75ca
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 15:55:02 +0000

    add OV-Ip-pkc (customSmallerIsBetter) benchmark result for 89e0a01cd18ab08acfa69aa74b6c459f87b42050

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745769277302,
+  "lastUpdate": 1745769302571,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L1": [
@@ -3072,6 +3072,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "OV-Is-pkc verify",
             "value": 217004,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ],
+    "OV-Ip-pkc": [
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "89e0a01cd18ab08acfa69aa74b6c459f87b42050",
+          "message": "sig_speed auto-push on\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-27T17:28:43+02:00",
+          "tree_id": "7401baf8d56e87f41fd37d2bc1d5052aba4c7945",
+          "url": "https://github.com/pablo-gf/liboqs/commit/89e0a01cd18ab08acfa69aa74b6c459f87b42050"
+        },
+        "date": 1745769301691,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "OV-Ip-pkc keypair",
+            "value": 1564970,
+            "unit": "cycles"
+          },
+          {
+            "name": "OV-Ip-pkc sign",
+            "value": 58859,
+            "unit": "cycles"
+          },
+          {
+            "name": "OV-Ip-pkc verify",
+            "value": 175702,
             "unit": "cycles"
           }
         ]
