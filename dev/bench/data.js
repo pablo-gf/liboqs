fe91d1ea65d7cc204e1952b698ecaa319c92cc18
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 16:14:51 +0000

    add sntrup761 (customSmallerIsBetter) benchmark result for 781af869157db7541100d008135143b25c3d69f7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745770466244,
+  "lastUpdate": 1745770491495,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L1": [
@@ -3039,6 +3039,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "sntrup761 decaps",
             "value": 32486,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "781af869157db7541100d008135143b25c3d69f7",
+          "message": "cleaned sig-bench\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-27T18:02:40+02:00",
+          "tree_id": "2f686f0987c9203d98ad9a4baf678ebb30bb1525",
+          "url": "https://github.com/pablo-gf/liboqs/commit/781af869157db7541100d008135143b25c3d69f7"
+        },
+        "date": 1745770490226,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sntrup761 keygen",
+            "value": 695436,
+            "unit": "cycles"
+          },
+          {
+            "name": "sntrup761 encaps",
+            "value": 29696,
+            "unit": "cycles"
+          },
+          {
+            "name": "sntrup761 decaps",
+            "value": 32571,
             "unit": "cycles"
           }
         ]
