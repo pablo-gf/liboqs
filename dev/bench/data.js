8bf030ad573403e6477469a9cea1e56f45c614c8
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 15:43:37 +0000

    add FrodoKEM-1344-SHAKE (customSmallerIsBetter) benchmark result for 89e0a01cd18ab08acfa69aa74b6c459f87b42050

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745768608938,
+  "lastUpdate": 1745768617672,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L1": [
@@ -2047,6 +2047,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "MAYO-3 verify",
             "value": 404162,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ],
+    "FrodoKEM-1344-SHAKE": [
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "89e0a01cd18ab08acfa69aa74b6c459f87b42050",
+          "message": "sig_speed auto-push on\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-27T17:28:43+02:00",
+          "tree_id": "7401baf8d56e87f41fd37d2bc1d5052aba4c7945",
+          "url": "https://github.com/pablo-gf/liboqs/commit/89e0a01cd18ab08acfa69aa74b6c459f87b42050"
+        },
+        "date": 1745768616900,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "FrodoKEM-1344-SHAKE keygen",
+            "value": 10148488,
+            "unit": "cycles"
+          },
+          {
+            "name": "FrodoKEM-1344-SHAKE encaps",
+            "value": 10608699,
+            "unit": "cycles"
+          },
+          {
+            "name": "FrodoKEM-1344-SHAKE decaps",
+            "value": 10447521,
             "unit": "cycles"
           }
         ]
