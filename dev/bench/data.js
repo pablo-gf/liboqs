550d65eb298fe76735523b422a3166b4fbc980b9
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 15:37:54 +0000

    add SPHINCS+-SHA2-192f-simple (customSmallerIsBetter) benchmark result for 89e0a01cd18ab08acfa69aa74b6c459f87b42050

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745768260507,
+  "lastUpdate": 1745768274251,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L1": [
@@ -1022,6 +1022,47 @@ window.BENCHMARK_DATA = {
           {
             "name": "Classic-McEliece-8192128f decaps",
             "value": 254621,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ],
+    "SPHINCS+-SHA2-192f-simple": [
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "89e0a01cd18ab08acfa69aa74b6c459f87b42050",
+          "message": "sig_speed auto-push on\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-27T17:28:43+02:00",
+          "tree_id": "7401baf8d56e87f41fd37d2bc1d5052aba4c7945",
+          "url": "https://github.com/pablo-gf/liboqs/commit/89e0a01cd18ab08acfa69aa74b6c459f87b42050"
+        },
+        "date": 1745768273794,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SPHINCS+-SHA2-192f-simple keypair",
+            "value": 1352386,
+            "unit": "cycles"
+          },
+          {
+            "name": "SPHINCS+-SHA2-192f-simple sign",
+            "value": 36138108,
+            "unit": "cycles"
+          },
+          {
+            "name": "SPHINCS+-SHA2-192f-simple verify",
+            "value": 2485027,
             "unit": "cycles"
           }
         ]
