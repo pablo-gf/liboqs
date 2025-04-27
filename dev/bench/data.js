4b8cf3b6fd8b26ea76bdfa7c665181e3b8bb1d04
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 17:15:02 +0000

    add SPHINCS+-SHA2-192s-simple (customSmallerIsBetter) benchmark result for 02ec569c088c8f7a14e3602685faf72f73aa0d66

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745774081969,
+  "lastUpdate": 1745774102225,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L1": [
@@ -3251,6 +3251,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "SPHINCS+-SHA2-192s-simple verify",
             "value": 1005185,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "02ec569c088c8f7a14e3602685faf72f73aa0d66",
+          "message": "cleaned kem-bench\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-27T19:05:19+02:00",
+          "tree_id": "ce6b6bfa03f4ff3a38999ba463955eb719111968",
+          "url": "https://github.com/pablo-gf/liboqs/commit/02ec569c088c8f7a14e3602685faf72f73aa0d66"
+        },
+        "date": 1745774101466,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SPHINCS+-SHA2-192s-simple keypair",
+            "value": 84982661,
+            "unit": "cycles"
+          },
+          {
+            "name": "SPHINCS+-SHA2-192s-simple sign",
+            "value": 806879654,
+            "unit": "cycles"
+          },
+          {
+            "name": "SPHINCS+-SHA2-192s-simple verify",
+            "value": 993862,
             "unit": "cycles"
           }
         ]
