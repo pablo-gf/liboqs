5936074e2a8cb6e426481a72ce695d61a521cc2b
github-action-benchmark <github@users.noreply.github.com>
2025-04-27 17:15:35 +0000

    add Kyber768 (customSmallerIsBetter) benchmark result for 02ec569c088c8f7a14e3602685faf72f73aa0d66

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745774129659,
+  "lastUpdate": 1745774135601,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L1": [
@@ -3409,6 +3409,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "Kyber768 decaps",
             "value": 35579,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "02ec569c088c8f7a14e3602685faf72f73aa0d66",
+          "message": "cleaned kem-bench\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-27T19:05:19+02:00",
+          "tree_id": "ce6b6bfa03f4ff3a38999ba463955eb719111968",
+          "url": "https://github.com/pablo-gf/liboqs/commit/02ec569c088c8f7a14e3602685faf72f73aa0d66"
+        },
+        "date": 1745774134605,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Kyber768 keygen",
+            "value": 39646,
+            "unit": "cycles"
+          },
+          {
+            "name": "Kyber768 encaps",
+            "value": 48194,
+            "unit": "cycles"
+          },
+          {
+            "name": "Kyber768 decaps",
+            "value": 35635,
             "unit": "cycles"
           }
         ]
