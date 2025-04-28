4d0241793d5b08cdfe0782eb00bb4d4f4a75f543
github-action-benchmark <github@users.noreply.github.com>
2025-04-28 07:05:37 +0000

    add MAYO-3 (customSmallerIsBetter) benchmark result for 5321715877d7831bcc7caea19f0180c0cf0180eb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745823910301,
+  "lastUpdate": 1745823937334,
   "repoUrl": "https://github.com/pablo-gf/liboqs",
   "entries": {
     "BIKE-L1": [
@@ -7741,6 +7741,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "MAYO-3 verify",
             "value": 406593,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "committer": {
+            "email": "pablogf@uma.es",
+            "name": "Pablo Gutiérrez Félix",
+            "username": "pablo-gf"
+          },
+          "distinct": true,
+          "id": "5321715877d7831bcc7caea19f0180c0cf0180eb",
+          "message": "increased alert-threshold\n\nSigned-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>",
+          "timestamp": "2025-04-28T08:46:11+02:00",
+          "tree_id": "340dc3a7856f2525d5d3e24246928886254026d2",
+          "url": "https://github.com/pablo-gf/liboqs/commit/5321715877d7831bcc7caea19f0180c0cf0180eb"
+        },
+        "date": 1745823936484,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "MAYO-3 keypair",
+            "value": 377047,
+            "unit": "cycles"
+          },
+          {
+            "name": "MAYO-3 sign",
+            "value": 953420,
+            "unit": "cycles"
+          },
+          {
+            "name": "MAYO-3 verify",
+            "value": 403388,
             "unit": "cycles"
           }
         ]
