177e60d7d056d3cff31cb955aa7f4716d44c7649
Pablo Gutiérrez Félix <pablogf@uma.es>
2026-02-25 12:11:29 +0100

    interactive input option fix
    Signed-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>

--- tests/ct_tooling/tools/memsan/ct_test.sh
+++ tests/ct_tooling/tools/memsan/ct_test.sh
@@ -39,7 +39,6 @@ build() {
 
 # Define a cleanup function that will restore the original test files with the backups
 cleanup() {
-    echo "Restoring original test files..."
     mv "$LIBOQS_DIR/tests/CMakeLists.txt.bak" "$LIBOQS_DIR/tests/CMakeLists.txt" 2>/dev/null || true
     mv "$LIBOQS_DIR/tests/test_kem.c.bak" "$LIBOQS_DIR/tests/test_kem.c" 2>/dev/null || true
     mv "$LIBOQS_DIR/tests/test_sig.c.bak" "$LIBOQS_DIR/tests/test_sig.c" 2>/dev/null || true
