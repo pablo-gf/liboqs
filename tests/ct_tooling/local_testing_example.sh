31fdd62f51ed3620be6f52a583bac84b9e6a9578
Pablo Gutiérrez Félix <pablogf@uma.es>
2026-02-26 13:38:49 +0100

    Unified into a single shell script
    Signed-off-by: Pablo Gutiérrez Félix <pablogf@uma.es>

--- tests/ct_tooling/local_testing_example.sh
+++ tests/ct_tooling/local_testing_example.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 
+# This script iterates through the compilation options provided by the framework to execute constant time tests
+# Adjust valgrind-varlat/memsan accordingly (note that gcc uses -fno-tree-vectorize while clang uses -fno-vectorize)
+
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -9,9 +12,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 for compiler_version in gcc gcc-14 clang clang-20; do
     # Iterate through both liboqs builds: generic vs. optimized
     for liboqs_build in generic auto; do
-        # Iterate through the different optimization flags (gcc uses -fno-tree-vectorize instead of -fno-vectorize) and execute tests asynchronously
+        # Iterate through the different optimization flags and execute tests asynchronously
         for opt_flag in -O0 -O1 -O2 -O3 -Os -Ofast "-O2 -fno-tree-vectorize" "-O3 -fno-tree-vectorize"; do
-            ./ct_test.sh "$compiler_version" "$liboqs_build" "$opt_flag" all
+            ./ct_test.sh valgrind-varlat "$compiler_version" "$liboqs_build" "$opt_flag" all
         done
     done
 done
